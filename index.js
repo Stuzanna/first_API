abf84d750ac2a510495c34eba466ce852ee714bd
Stuart Mould <smould1011@gmail.com>
2022-02-06 16:22:46 +0000

    Created POST endpoint but needs middleware due to express

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.listen(
 )
 
 
-//This is an endpoint you're setting up where the tshirt is.
+/*This is an endpoint you're setting up where the tshirt is.
 
-//If you just had the first arg before the comma, would set up our
-//server with that endpoint. We then have to handle a request to it.
-//Which we do by adding in the callback (the 2nd arg) which will fire.
-//This response function gives access to two objects, request, response
+If you just had the first arg before the comma, would set up our
+server with that endpoint. We then have to handle a request to it.
+Which we do by adding in the callback (the 2nd arg) which will fire.
+This response function gives access to two objects, request, response*/
 
 app.get('/tshirt', (req, res) => {
     res.status(200).send({
@@ -23,4 +23,26 @@ app.get('/tshirt', (req, res) => {
         size: 'large'
     })
     
+});
+
+/*This is another endpoint.
+
+This has a route parameter, captures dynamic values in the URL. The ID
+of the tshirt, there may be millions on this API.
+Post is creating new info, so new tshirt in this case.
+Req allows us to access the info in the request.
+
+*/
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.send({
+        tshirt: `tshirt with your ${logo} and ID of ${id}`,
+    });
+
 });
