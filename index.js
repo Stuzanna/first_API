f008ae0ce2bb603a3e03de1347bc5f54396808cb
Stuart Mould <smould1011@gmail.com>
2022-02-06 15:49:39 +0000

    API will now start with feedback

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const app = require('express')();
-const PORT = 8080
+const app = require('express')(); //App is the API we're building
+//Import the Express package, brackets to intialise it
+const PORT = 8080; //Defined endpoint for the API
 
+//Listen will fire up the API, listen on a port
 app.listen(
-    PORT
+    PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`)
-)
+//Optinal callback to let us know when it's ready
+    )
