2c0fe9a43a214c680f4b061553c92ae34f3a6f17
Stuart Mould <smould1011@gmail.com>
2022-02-06 16:07:39 +0000

    Created endpoint for tshirt

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ app.listen(
     () => console.log(`it's alive on http://localhost:${PORT}`)
 //Optional callback to let us know when it's ready
 )
+
+
+//This is an endpoint you're setting up where the tshirt is.
+
+//If you just had the first arg before the comma, would set up our
+//server with that endpoint. We then have to handle a request to it.
+//Which we do by adding in the callback (the 2nd arg) which will fire.
+//This response function gives access to two objects, request, response
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'tshirt',
+        size: 'large'
+    })
+    
+});
